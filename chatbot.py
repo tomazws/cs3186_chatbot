47231654d8f89f59078ae1d1a0bf39b57f4492ee
tomazws <tomazws@mac.com>
2024-03-10 22:34:28 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -43,20 +43,22 @@ if prompt := st.chat_input('Ask me anything about CS 3186'):
         st.write(response)
         if response.finish_reason == 'tool_calls':
             st.write(response.message.tool_calls[0].function.name)
-            response = call_function(response.message.tool_calls[0].function)
+            asdfasdf()
+            #response = call_function(response.message.tool_calls[0].function)
         else:
             st.write(response.message.content)
     #st.session_state.messages.append({'role': 'assistant', 'content': response})
 
 # Functions for OpenAI's function calling method
-def call_function(bla):
-    if bla.name == 'createDiagram':
-        try:
-            parsed_args = json.loads(bla.arguments)
-            st.write(parsed_args)
-        except Exveption as e:
-            return f'Function execution failed: {e}'
-    return 'no'
+def asdfasdf():
+    st.write('yasdf')
+    # if bla.name == 'createDiagram':
+    #     try:
+    #         parsed_args = json.loads(bla.arguments)
+    #         st.write(parsed_args)
+    #     except Exveption as e:
+    #         return f'Function execution failed: {e}'
+    # return 'no'
 
 def createDiagram(dot_script):
     return 'Yes yes yall'
