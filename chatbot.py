24004458ba7ff1cc8b37e38286f17b1db205f493
tomazws <tomazws@mac.com>
2024-03-10 18:23:40 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,8 @@ import streamlit as st
 import openai
 import time
 
-with st.chat_message('user'):
-    st.write('Hello!')
-
 openai.api_key = st.secrets['OPENAI_API_KEY']
+
+prompt = st.chat_input('Ask me anything about CS 3186')
+if prompt:
+    st.write(f'/user has sent the following prompt: {prompt}')
