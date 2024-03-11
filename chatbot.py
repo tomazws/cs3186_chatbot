6f14cd7b5149548720087bbbd57e8b0b897fbd45
tomazws <tomazws@mac.com>
2024-03-10 18:36:22 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -16,6 +16,20 @@ for message in st.session_state.messages:
 
 # React to user input
 if prompt := st.chat_input('Ask me anything about CS 3186'):
+    # Display user message in chat message container
     with st.chat_message('user'):
         st.markdown(prompt)
+
+    # Add user message to chat history
     st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+    response = f'Echo: {prompt}'
+
+    # Display assistant response in chat message container
+    with st.chat_message('assistant'):
+        st.markdown(response)
+
+    # Add assistant response to chat history
+    st.session_state.messages.append({'role': 'assistant', 'content': response})
+    
+
