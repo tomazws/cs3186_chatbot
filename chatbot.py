6bcb8a3adedd88c6e93ab9ea9541a276905309e0
tomazws <tomazws@mac.com>
2024-03-10 18:42:08 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -1,35 +1,26 @@
 import streamlit as st
-import openai
 
-openai.api_key = st.secrets['OPENAI_API_KEY']
-
-#st.title('CS 3186 Student Assistant Chatbot')
+st.title("Echo Bot")
 
 # Initialize chat history
-if 'messages' not in st.session_state:
+if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message['role']):
-        st.markdown(message['content'])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # React to user input
-if prompt := st.chat_input('Ask me anything about CS 3186'):
+if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
-    with st.chat_message('user'):
-        st.markdown(prompt)
-
+    st.chat_message("user").markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({'role': 'user', 'content': prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f'Echo: {prompt}'
-
+    response = f"Echo: {prompt}"
     # Display assistant response in chat message container
-    with st.chat_message('assistant'):
+    with st.chat_message("assistant"):
         st.markdown(response)
-
     # Add assistant response to chat history
-    st.session_state.messages.append({'role': 'assistant', 'content': response})
-
-
+    st.session_state.messages.append({"role": "assistant", "content": response})
