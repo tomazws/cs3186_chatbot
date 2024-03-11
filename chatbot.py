276c5cf590c936267cf099cb8ae082c3cd109c72
tomazws <tomazws@mac.com>
2024-03-10 18:38:53 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ import openai
 
 openai.api_key = st.secrets['OPENAI_API_KEY']
 
-st.title("CS 3186 Student Assistant Chatbot")
+st.title('asdf')
 
 # Initialize chat history
 if 'messages' not in st.session_state:
