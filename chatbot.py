17a2b0c25c537f2f553a67b4569dcb6e150859ce
tomazws <tomazws@mac.com>
2024-03-10 19:09:18 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -28,14 +28,15 @@ if prompt := st.chat_input('Ask me anything about CS 3186'):
     st.session_state.messages.append({'role': 'user', 'content': prompt})
 
     # Display assistant response in chat message container
-    with st.chat_message('assistant'):
-        stream = client.chat.completions.create(
-            model = st.session_state['openai_model'],
-            messages = [
-                {'role': m['role'], 'content': m['content']}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.spinner():
+        with st.chat_message('assistant'):
+            stream = client.chat.completions.create(
+                model = st.session_state['openai_model'],
+                messages = [
+                    {'role': m['role'], 'content': m['content']}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
