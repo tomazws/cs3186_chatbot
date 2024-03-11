26888d53def9a932312cc0f1ca9268f502bb00f0
tomazws <tomazws@mac.com>
2024-03-10 18:31:09 -0700

    streamlit openai tutorial

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import openai
-import time
 
 openai.api_key = st.secrets['OPENAI_API_KEY']
 
-prompt = st.chat_input('Ask me anything about CS 3186')
-if prompt:
-    st.write(f'/user has sent the following prompt: {prompt}')
+st.title = 'CS 3186 Student Assistant Chatbot'
+
+# Initialize chat history
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+# React to user input
+if prompt := st.chat.input('Ask me anything about CS 3186'):
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    st.session_state.messages.append({'role': 'user', 'content': prompt})
